4f383cc6c6fdc4bcf1bbdec934d167f959b5dfa1
megangoodland <megangoodland@gmail.com>
2019-02-22 15:43:40 -0500

    Update ode_solving.cpp

--- ode_solving.cpp
+++ ode_solving.cpp
@@ -1,7 +1,7 @@
 // we solve the simple ODE x’ = 3/(2t^2) + x/(2t) * with initial condition x(1) = 0.
 // Analytic solution is x(t) = sqrt(t) - 1/t 
 
-#include <iostream >
+#include <iostream>
 #include <boost/numeric/odeint.hpp> using namespace boost::numeric::odeint;
 
 void rhs(const double x, double &dxdt, const double t) { 
