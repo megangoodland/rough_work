deb7efbab008a822b657a0cc41d6b2f18c747bfa
megangoodland <megangoodland@gmail.com>
2019-02-26 13:44:45 -0500

    Update ode_solving.cpp

--- ode_solving.cpp
+++ ode_solving.cpp
@@ -14,9 +14,26 @@
 #include <iostream>
 #include <boost/numeric/odeint.hpp>
 #include <boost/array.hpp>
+#include <netcdfcpp.h>
+#include <rarray>
+#include <rarrayio>
 using namespace boost::numeric::odeint;
 using namespace std;
 
+
+// write data to the netCDF file
+// function here
+    
+// Create the file.
+NcFile dataFile("pres_temp_4D.nc", NcFile::Replace);
+// Define dimensions
+// One dimension for the S K Z data
+// One dimension for time
+// One dimension for the two result sets
+NcDim *lvlDim, *latDim, *lonDim, *recDim;
+latDim = dataFile.add_dim("latitude", NLAT);
+lonDim = dataFile.add_dim("longitude", NLON);
+recDim = dataFile.add_dim("time"); //this is an unlimited dimension
 
 // defining constants and initial conditions
 const double A = 0.03;  // rate at which zombies are killed by K
@@ -27,21 +44,25 @@ const double K0 = 9; // number of people who can kill zombies
 double Z0 = 263; // number of zombies: will be varying this value
 double S0 = 491 - Z0; // number of regular people who can't kill zombies
 double num = 50; // number of steps to perform
+int i = 0;
 
-
+// defining boost array to hold x = ( S, K, Z )
 typedef boost::array< double , 3 > state_type;
-
-
-
-void print( const state_type &x , const double t )
-{
-    cout << t << '\t' << x[0] << '\t' << x[1] << '\t' << x[2] << endl;
     
+// defining 3D array to hold data history
+// dimensions: tSKZ, time, 2 saves
+rarray<double,3> history(4,num,2);
+
+// saves data in array
+void add_to_array(const state_type &x , const double t){
+    cout << t << '\t' << x[0] << '\t' << x[1] << '\t' << x[2] << endl; 
+    i = i++; // increment
 }
 
+
+
 // x = ( S, K, Z )
-void zombie_odes( const state_type &x , state_type &dxdt , double t )
-{
+void zombie_odes( const state_type &x , state_type &dxdt , double t ){
     dxdt[0] = -(B*x[0]*x[2])-(E*x[0]*x[1]);
     dxdt[1] = -(C*x[1]*x[2])+(E*x[0]*x[1]);
     dxdt[2] = -(B*x[0]*x[2])+(C*x[1]*x[2])-(A*x[1]*x[2]);
@@ -52,6 +73,6 @@ int main() {
   state_type x = {S0, K0, Z0}; // initial conditions
   // integrate needs (system, x0, t0, t1, dt, observer)
   int n; //n is the number of steps performed
-  n = integrate(zombie_odes , x , 0.0 , num , 0.01 , print);
+  n = integrate(zombie_odes , x , 0.0 , num , 0.01 , add_to_array);
   cout << n << endl;
 }
