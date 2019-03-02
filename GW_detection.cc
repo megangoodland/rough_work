010374c8d1656e021e8c7b562f3ebd4be62af3fd
megangoodland <megangoodland@gmail.com>
2019-03-02 16:38:12 -0500

    Update GW_detection.cc

--- GW_detection.cc
+++ GW_detection.cc
@@ -28,7 +28,7 @@ using namespace std;
 using namespace netCDF;
 int main(){
   NcFile file("detection01.nc", NcFile::read); // selects the file to read
-  NcDim ntDim = file.getDim('nt');
+  NcDim ntDim = file.getDim("nt");
   int nt_size = ntDim.getSize(); // size of data
 
   NcVar data = file.getVar("f"); // Get variable named 'f'
