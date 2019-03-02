119192ed97a7746fb2ea13174b429504ee5a7ab6
megangoodland <megangoodland@gmail.com>
2019-03-02 16:08:33 -0500

    Create GW_detection.cc

---
+++ GW_detection.cc
@@ -0,0 +1,29 @@
+// GW_detection.cc
+// 
+// This file will...
+// 1. Read the predicted GW signal from GWprediction.nc.
+// 2. Read one of the GW signal from observations detection01.nc . . . detection32.nc.
+// 3. Compute the FFT of the two complex quantities, using FFTW.
+// 4. Compute the power spectrum of both signals.
+// 5. Compute the correlation coefficient between the power spectra using a .BLAS call for the inner product
+// 6. Output the correlation coefficient
+// 7. Repeat steps 2-to-6 for each of the signals in the observation set.
+// 8. Finally, determine the 5 most significant candidates (those with the 5 largest values of the correlation coefficient) from the observations set.
+//
+// Compile with 'make'.
+// On the clusters, it will be necessary to load the "gcc" module first.
+//
+// Run with
+//
+//     ./GW_detection
+// 
+// Megan Goodland, March 2019, for Assignment 6 of PHY1610 at University of Toronto
+
+
+#include <rarray>
+#include <netcdf>
+#include <complex>
+
+netCDF::NcFile file("detection01.nc", netCDF::NcFile::read); // selects the file to read
+rarray<std::complex<double>,1> f(file.getDim("nt").getSize());
+file.getVar("f").getVar(&f[0]);
