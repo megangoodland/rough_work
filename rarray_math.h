c89d592fb4acfb6ecc46efafc6233fa1252f5832
megangoodland <megangoodland@gmail.com>
2019-03-03 14:37:30 -0500

    Create rarray_math.h

---
+++ rarray_math.h
@@ -0,0 +1,31 @@
+// Header for the module for doing math functions on rarrays for GW_detection.cc
+#ifndef netCDF_reading_h
+#define netCDF_reading_h
+
+#include <iostream>
+#include <rarray>
+#include <rarrayio>
+#include <vector>
+#include <complex>
+#include <stdio.h>
+#include <fftw3.h> // for fft stuff
+#include <cmath> // for pow
+#include <cblas.h>
+using namespace std;
+
+// fft: Fast fourier transform function for 1D complex double rarray
+//      Input: f rarray
+//      Output: fhat rarray
+rarray<complex<double>,1> fft(rarray<complex<double>,1>& f);
+
+// sq_norm: This function returns an rarray of the square norms of the complex values in the rarray input
+//      Input: 1D complex double rarray
+//      Output: 1D double rarray
+rarray<double,1> sq_norm(rarray<complex<double>,1>& fhat);
+
+// correlation: This function calculates the correlation between two 1D rarrays via C_FG = <F,G> / sqrt(<F,F><G,G>)
+//      Input: Two 1D double rarrays
+//      Output: A double value
+double correlation(rarray<double,1>& Fk,rarray<double,1>& Gk);
+
+#endif /* rarray_math */
