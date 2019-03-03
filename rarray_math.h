123d07a71acb40b9b3d8204de70d3ac033ff6eda
megangoodland <megangoodland@gmail.com>
2019-03-03 15:06:58 -0500

    Update rarray_math.h

--- rarray_math.h
+++ rarray_math.h
@@ -2,6 +2,16 @@
 #ifndef netCDF_reading_h
 #define netCDF_reading_h
 
+#include <iostream>
+#include <rarray>
+#include <rarrayio>
+#include <vector>
+#include <netcdf> // for file i/o
+#include <complex>
+
+using namespace std;
+using namespace netCDF;
+
 // fft: Fast fourier transform function for 1D complex double rarray
 //      Input: f rarray
 //      Output: fhat rarray
