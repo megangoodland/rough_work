85dfa5378340809e91edc31ce446ae78ffbca1d6
megangoodland <megangoodland@gmail.com>
2019-03-03 14:52:59 -0500

    Update GW_detection.cc

--- GW_detection.cc
+++ GW_detection.cc
@@ -19,18 +19,18 @@
 // 
 // Megan Goodland, March 2019.
 // Assignment 6 of PHY1610 at University of Toronto
-#include "rarray_math.h"
-#include "netCDF_reading.h"
+
 #include <iostream>
 #include <rarray>
 #include <rarrayio>
 #include <vector>
 #include <complex>
 #include <stdio.h>
-
+#include "rarray_math.h"
+#include "netCDF_reading.h"
 
 using namespace std;
-using namespace netCDF;
+
 
 
 
